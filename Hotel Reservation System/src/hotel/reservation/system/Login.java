396be899cbe31ea20289ae8a792a9b3169f247a2
MarkLaurenceSalud <138182143+MarkLaurenceSalud@users.noreply.github.com>
2023-06-30 21:33:49 +0800

    Add files via upload

--- Hotel Reservation System/src/hotel/reservation/system/Login.java
+++ Hotel Reservation System/src/hotel/reservation/system/Login.java
@@ -17,17 +17,23 @@ class MainPage extends JFrame implements ActionListener{
     
     
     JComboBox cb1, cb2;
-    JLabel l1;
+    JLabel l1, l2;
+    JButton b1, b2, b3, b4, b5;
     
   MainPage(){
     setResizable(false);
     setLayout(null);
     setDefaultCloseOperation(EXIT_ON_CLOSE);
     
-     l1 = new JLabel("Hello Client");
-     l1.setBounds(710, 20, 100, 30);
+     l1 = new JLabel("Hello Client!");
+     l1.setBounds(900, 20, 100, 30);
      l1.setFont(new Font("Times New Roman", Font.BOLD, 15));
      add(l1);
+     
+     l2 = new JLabel("Welcome to our Hotel");
+     l2.setFont(new Font("Times New Roman", Font.BOLD, 35));
+     l2.setBounds(380, 100, 350, 30);
+     add(l2);
        
      String[] Booking = {"Booking","Add Booking","My Booking"};
      cb1 = new JComboBox(Booking); 
@@ -41,7 +47,36 @@ class MainPage extends JFrame implements ActionListener{
      cb2.addActionListener(this);
      add(cb2);
      
+     b1 = new JButton("Check in");
+     b1.setFont(new Font("Rockwell", Font.BOLD, 30));
+     b1.setForeground(Color.gray);
+     b1.setBounds(100, 200, 300, 60);
+     
+     b2 = new JButton("Room Booking");
+     b2.setFont(new Font("Rockwell", Font.BOLD, 30));
+     b2.setForeground(Color.gray);
+     b2.setBounds(410, 200, 300, 60);
+     
+     b3 = new JButton("Rooms");
+     b3.setFont(new Font("Rockwell", Font.BOLD, 30));
+     b3.setForeground(Color.gray);
+     b3.setBounds(720, 200, 300, 60);
+     
+     b4 = new JButton("Cancel Booking");
+     b4.setFont(new Font("Rockwell", Font.BOLD, 30));
+     b4.setForeground(Color.gray);
+     b4.setBounds(250, 350, 300, 60);
+     
+     b5 = new JButton("Check Out");
+     b5.setFont(new Font("Rockwell", Font.BOLD, 30));
+     b5.setForeground(Color.gray);
+     b5.setBounds(570, 350, 300, 60);
       
+     add(b1); 
+     add(b2); 
+     add(b3); 
+     add(b4); 
+     add(b5); 
   }
 
     @Override
@@ -153,8 +188,7 @@ public class Login extends JFrame{
             @Override
             public void actionPerformed(ActionEvent ae){
                if(t1.getText().toString().equals("admin")&& t2.getText().toString().equals("admin"))
-               {
-                   
+               {                  
                    dispose();
                    MainPage mp = new MainPage();
                    mp.setVisible(true);
